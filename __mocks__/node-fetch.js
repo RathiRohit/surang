eb40243eb2f961d22d7762ec3cbf981906d92943
Rohit Rathi <rathirohitg1997@gmail.com>
2021-01-05 02:51:36 +0530

    fix json body bug in transformer

--- __mocks__/node-fetch.js
+++ __mocks__/node-fetch.js
@@ -4,9 +4,11 @@ fetch.mockImplementation(() => Promise.resolve({
   status: 200,
   statusText: 'SUCCESS',
   type: 'application/json',
-  getHeaders: () => ({
+  headers: {
     'set-cookie': 'TEST_COOKIE_HEADER',
-  }),
+    keys: () => ['set-cookie'],
+    get: () => 'TEST_COOKIE_HEADER',
+  },
   text: () => Promise.resolve({
     testKey1: 'testValue1',
     testKey2: 2,
