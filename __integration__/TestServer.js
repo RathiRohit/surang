92767e939b0ef9ff3569b5c9eacdbae722318253
Rohit Rathi <rathirohitg1997@gmail.com>
2021-01-27 20:10:23 +0530

    add more integration tests for GET json, file & POST

--- __integration__/TestServer.js
+++ __integration__/TestServer.js
@@ -6,6 +6,8 @@ jest.unmock('cookie-parser');
 const express = require('express');
 const cookieParser = require('cookie-parser');
 
+const getJsonResponse = require('./responses/get-json.json');
+
 class TestServer {
   start(port) {
     return new Promise((resolve, reject) => {
@@ -18,6 +20,18 @@ class TestServer {
 
         this.app.get('/get-html', (req, res) => {
           res.sendFile('responses/get-html.html', { root: __dirname });
+        });
+
+        this.app.get('/get-json', (req, res) => {
+          res.json(getJsonResponse);
+        });
+
+        this.app.get('/get-file', (req, res) => {
+          res.sendFile('responses/get-file.css', { root: __dirname });
+        });
+
+        this.app.post('/post-empty', (req, res) => {
+          res.sendStatus(req.body.requestData?.isValid ? 200 : 400);
         });
 
         this.server = this.app.listen(port, () => {
--- __integration__/integration.test.js
+++ __integration__/integration.test.js
@@ -6,10 +6,13 @@ jest.unmock('ws');
 
 jest.setTimeout(60000);
 
+const fs = jest.requireActual('fs-extra');
 const fetch = jest.requireActual('node-fetch');
 
 const TestServer = require('./TestServer');
 const Surang = require('../surang');
+
+const getJsonResponse = require('./responses/get-json.json');
 
 const SURANG_SERVER_AUTH_KEY = 'something-secret';
 const SURANG_SERVER_PORT = 7000;
@@ -23,6 +26,11 @@ function getHeaders(response) {
   return headers;
 }
 
+/*
+* These tests are supposed to run on CI environment and assumes
+* surang-server (with appropriate version) is running on test
+* environment on 7000 port with 'something-secret' as auth key.
+*/
 describe('surang tunnel', () => {
   let surang;
   let testServer;
@@ -72,5 +80,86 @@ describe('surang tunnel', () => {
     expect(getHeaders(surangResponse)['content-type']).toBe('text/html; charset=utf-8');
     expect(getHeaders(surangResponse)).toEqual(getHeaders(normalResponse));
     expect(await surangResponse.text()).toEqual(await normalResponse.text());
+  });
+
+  it('should forward GET request and give back correct JSON response', async () => {
+    const request = {
+      method: 'GET',
+      headers: {
+        'cache-control': 'no-cache',
+        'user-agent': 'test-user-agent',
+      },
+      cookies: {
+        TEST_COOKIE: 'test-cookie',
+      },
+    };
+
+    const surangResponse = await fetch(`http://localhost:${SURANG_SERVER_PORT}/get-json`, request);
+    const jsonResponse = await surangResponse.json();
+    const normalResponse = await fetch(`http://localhost:${TEST_SERVER_PORT}/get-json`, request);
+
+    expect(surangResponse.status).toBe(200);
+    expect(surangResponse.status).toBe(normalResponse.status);
+    expect(surangResponse.statusText).toBe('OK');
+    expect(surangResponse.statusText).toBe(normalResponse.statusText);
+    expect(getHeaders(surangResponse)['content-type']).toBe('application/json; charset=utf-8');
+    expect(getHeaders(surangResponse)).toEqual(getHeaders(normalResponse));
+    expect(jsonResponse).toEqual(getJsonResponse);
+    expect(jsonResponse).toEqual(await normalResponse.json());
+  });
+
+  it('should forward GET request and give back correct FILE response', async () => {
+    const request = {
+      method: 'GET',
+      headers: {
+        'cache-control': 'no-cache',
+        'user-agent': 'test-user-agent',
+      },
+      cookies: {
+        TEST_COOKIE: 'test-cookie',
+      },
+    };
+
+    const surangResponse = await fetch(`http://localhost:${SURANG_SERVER_PORT}/get-file`, request);
+    const fileResponse = new Uint8Array(await surangResponse.arrayBuffer());
+    const expectedFileResponse = new Uint8Array(fs.readFileSync('./responses/get-file.css', null).buffer);
+    const normalResponse = await fetch(`http://localhost:${TEST_SERVER_PORT}/get-file`, request);
+
+    expect(surangResponse.status).toBe(200);
+    expect(surangResponse.status).toBe(normalResponse.status);
+    expect(surangResponse.statusText).toBe('OK');
+    expect(surangResponse.statusText).toBe(normalResponse.statusText);
+    expect(getHeaders(surangResponse)['content-type']).toBe('text/css; charset=utf-8');
+    expect(getHeaders(surangResponse)).toEqual(getHeaders(normalResponse));
+    expect(fileResponse).toEqual(expectedFileResponse);
+    expect(fileResponse).toEqual(new Uint8Array(await normalResponse.arrayBuffer()));
+  });
+
+  it('should forward POST request and give back correct EMPTY response', async () => {
+    const request = {
+      method: 'POST',
+      headers: {
+        'cache-control': 'no-cache',
+        'user-agent': 'test-user-agent',
+        'content-type': 'application/json',
+      },
+      cookies: {
+        TEST_COOKIE: 'test-cookie',
+      },
+      body: JSON.stringify({ requestData: { isValid: true } }),
+    };
+
+    const surangResponse = await fetch(`http://localhost:${SURANG_SERVER_PORT}/post-empty`, request);
+    const plainResponse = await surangResponse.text();
+    const normalResponse = await fetch(`http://localhost:${TEST_SERVER_PORT}/post-empty`, request);
+
+    expect(surangResponse.status).toBe(200);
+    expect(surangResponse.status).toBe(normalResponse.status);
+    expect(surangResponse.statusText).toBe('OK');
+    expect(surangResponse.statusText).toBe(normalResponse.statusText);
+    expect(getHeaders(surangResponse)['content-type']).toBe('text/plain; charset=utf-8');
+    expect(getHeaders(surangResponse)).toEqual(getHeaders(normalResponse));
+    expect(plainResponse).toEqual('OK');
+    expect(plainResponse).toEqual(await normalResponse.text());
   });
 });
